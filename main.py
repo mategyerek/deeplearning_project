5429e72ea673da9b69e39c89d5e2867ce1e11e83
mategyerek <materadio@gmail.com>
2024-12-15 10:46:39 +0100

    lets try training

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import numpy as np
+import math
 
 import torch
 from torch.utils.data import DataLoader
+import torch.optim as optim
 from SegmentationDataset import SegmentationDataset
+from UNet import UNet
+from loss import ComboLoss
 
 import torchvision.transforms.functional as F
 import cv2
@@ -24,16 +28,52 @@ def show_img(tensor):  # grayscale
     plt.imshow(np.asarray(F.to_pil_image(tensor.detach())))
 
 
+def show_img_grid(l, nx=4, ny=None):
+    n = len(l)
+
+    if ny == None:
+        ny = math.ceil(n/nx)
+    elif ny * nx < n:
+        raise ValueError("Grid is too small. ny*nx must be larger than len(l)")
+    for idx, tensor in enumerate(l):
+        plt.subplot(ny, nx, idx + 1)
+        plt.axis('off')
+        show_img(tensor)
+    plt.show()
+
+
+def train(model, dataloader, optimizer, logging=False):
+    model.train()
+    gpu = torch.cuda.device(0)
+    loss_history = []
+    for i, batch in enumerate(dataloader):
+        x, y = batch[0].to(gpu), batch[1].to(gpu)
+        optimizer.zero_grad()
+        outputs = model(x)
+        loss = ComboLoss()(outputs, y)
+        loss_history.append(loss)
+        loss.backward()
+        optimizer.step()
+        if i % 10 == 0 and logging:
+            print(i, loss.item())
+    return model, np.average(loss_history)
+
+
 df = pd.read_json("_annotation.json")
 ds = SegmentationDataset("data", annotations=df,
                          target_transform=mask_to_tensor)
 
-dl = DataLoader(ds, 10)
+dl = DataLoader(ds, 12)
 
-x, y = next(iter(dl))
+"""x, y = next(iter(dl))
 
-plt.subplot(2, 1, 1)
-show_img(x[0])
-plt.subplot(2, 1, 2)
-show_img(y[0])
-plt.show()
+show_img_grid(x)
+"""
+
+unet_model = UNet(1, 1)
+optimizer = optim.Adam(unet_model.parameters(), lr=0.001)
+train_loss = []
+for e in 1:
+    unet_model, loss = train(unet_model, dl, optimizer, True)
+    train_loss.append(loss)
+    torch.save(unet_model.state_dict(), "last.pt")
